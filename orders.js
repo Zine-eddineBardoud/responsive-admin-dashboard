2a13fe78b2b8d45348f1ac20a4c94b28d9ba372d
Zine-eddine Bardoud <zineddinebardoud38@gmail.com>
2023-05-03 19:16:47 +0100

    first-commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'product 1',
+        productNumber: '011101',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'product 2',
+        productNumber: '022202',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'product 4',
+        productNumber: '044404',
+        paymentStatus: 'Due',
+        shipping: 'Delivred'
+    },
+    {
+        productName: 'product 3',
+        productNumber: '033303',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'product 5',
+        productNumber: '055505',
+        paymentStatus: 'Paid',
+        shipping: 'Delivred'
+    }
+]
